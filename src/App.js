916961fee4f9035792a8e9009213c85f8c849769
dogasudesonmez <82324445+dogasudesonmez@users.noreply.github.com>
2023-02-28 00:24:01 +0300

    Veri çekme, sayfa temelleri
    api dan veri çekildi. sayfa genel anlamıyla oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './app.css';
+import { FaSearch } from '@react-icons/all-files/fa/FaSearch';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [coins, setCoins] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+            .then(response => response.json())
+            .then(response => setCoins(response));
+    }, []);
+    return (
+        <div className="App">
+            <h1>Kripto Borsa </h1>
+
+                <input type="text"></input> <FaSearch />
+
+            <div id="list_block">
+                <p className="list_p">Isim</p>
+                <p className="list_p">Sembol</p>
+                <p className="list_p">Fiyat</p>
+                <p className="list_p">Degisim</p>
+
+            </div>
+
+            {coins.map(coin => {
+                return (
+                    <div key={coin.name} id="coin_columns">
+                        <div className="coin_symbl">
+                        <p><img src={coin.image} style={{ width: "30px" }} /></p>
+                            <h2>{coin.name}</h2>
+                        </div>
+                        <p style={{ fontSize: "15px" }}>{coin.symbol}</p>
+                        <p>${coin.current_price}</p>
+                        <p style={{ color: "deeppink" }}>{coin.price_change_percentage_24h}%</p>
+
+                </div>
+                );
+            })}
+
+
+        </div>
+
   );
 }
 
