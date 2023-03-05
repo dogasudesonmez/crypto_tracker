11709cfc6b33edc500c31999fcffb7758336f3dd
dogasudesonmez <82324445+dogasudesonmez@users.noreply.github.com>
2023-03-05 12:56:28 +0300

    Düzenleme
    input form eklendi ve aktifleştirildi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { useState, useEffect, TextField } from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
-import { FaSearch } from '@react-icons/all-files/fa/FaSearch';
+
+
 import Coin from './components/Coin';
 
-function App() {
 
+function App() {
+    const [coins, setCoins] = useState([]);
+    const [search, setSearch] = useState('');
+    useEffect(() => {
+        fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+            .then(response => response.json())
+            .then(response => setCoins(response));
+    }, []);
+    const handleChange = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const filteredCoins = coins.filter(coin =>
+        coin.name.toLowerCase().includes(search.toLowerCase())
+    )
     return (
         <div className="App">
             <h1>Kripto Borsa </h1>
-            
-           
-            <FaSearch />
 
-               
+            <form>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    className="coin-input" onChange={handleChange}
+                ></input>
+            </form>
+
+                          
             <div id="coin_columns">
                 <div id="head_line">
                 <h2>Isim</h2>
                 <h2>Sembol</h2>
                 <h2>Fiyat</h2>
-                    <h2>Degisim</h2>
+                <h2>Degisim</h2>
                 </div>
-                        <Coin />       
-                </div>
+                {filteredCoins.map(coin => {
+                    return (
+                        <Coin key={coin.id} name={coin.name} image={coin.image}
+                            symbol={coin.symbol} price={coin.current_price}
+                            priceChange = { coin.price_change_percentage_24h }
+                            
+                        />
 
+                    );
+                }
+                )}
+            </div>
+            
         </div>
 
   );
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,39 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import './coin.css';
 
-const Coin = () => {
+const Coin = ({ name, image, symbol, price, priceChange }) => {
     const [coins, setCoins] = useState([]);
-
+ 
     useEffect(() => {
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
             .then(response => response.json())
             .then(response => setCoins(response));
     }, []);
-
     return (
-        <div>
-            
-            {coins.map(coin => {
-                return (
-                    <div className="coin_lines">
-                        <div className="title_name" key={coin.name}>
-                            <p><img src={coin.image} style={{ width: "30px" }} /></p><h2>{coin.name}</h2>
-                            </div>                       
-                        <div key={coin.name} className="title_symbol">
-                            <p style={{ fontSize: "15px" }}>{coin.symbol}</p>
-                        </div>                            
-                        <div className="title_price" key={coin.name} >
-                            <p>${coin.current_price}</p>
-                        </div>
-                            
-                        <div className="title_change" key={coin.name}>
-                            <p style={{ color: "deeppink" }}>{coin.price_change_percentage_24h}%</p>
-                            </div>
 
-                    </div>
-                );
-            })}
-        </div>
+            <div className="coin_lines">
+                <div className="title_name">
+                    <img src={image} alt="crypto" style={{ widt: "50px", height: "50px" }} />
+                    <h1>{name}</h1>
+                    
+                </div>
+                <div>
+                <p >{symbol}</p>
+            </div>
+            <div>
+                <p >${price}</p>
+            </div>
+            <div>
+                <p > {priceChange}%</p>
+            </div>
+                   
+                
+                
+            </div>
+
+
     );
 }
 export default Coin;
